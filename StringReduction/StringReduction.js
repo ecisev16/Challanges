955ea73f0da6bc21f05c84ac49105a3ba771e344
Ezgi Sevi <ezgi.sevi16@gmail.com>
2022-09-10 20:36:19 +0300

    second step

--- StringReduction/StringReduction.js
+++ StringReduction/StringReduction.js
@@ -2,20 +2,44 @@
 
 function stringReduction(str){
 
+    let x;
     //let newStr = [];
     //let itemNo = 0;
 
-    if(str.length === 1){
+    if(str.length <= 1){
         newStr[itemNo] = str[0];
-        console.log("str", newStr);
+        console.log("last one", str[0]);
+        return newStr;
+        //console.log("str", newStr);
         //return newStr;    this is wrong because with this if statement, the fuction which in line #19 finished its job. So code move on with the line #20
     }else{
 
-        if(str[str.length-1] == str[str.length - 2]){
+        if(str[str.length - 1] == str[str.length - 2]){
             newStr[itemNo] = str[str.length-1];
             itemNo++;
-            str = str.slice(0, str.length - 1)
-            console.log(newStr);
+            str.pop();
+            //console.log(newStr);
+            stringReduction(str);
+            return newStr;
+        }else{
+            for(var i=0; i<3; i++){
+                if(str[str.length - 1] != abc[i]){
+                    if(str[str.length - 2] != abc[i]){
+                        
+                        //str.replace(str[str.length - 2], abc[i]);
+                        
+                        x = abc[i];
+                        
+                        console.log("changed one", x);
+                        console.log("abc", abc[i]);
+
+                    } 
+                }
+            }
+            str[str.length - 2] = x; //we couldn't whrite it in the for loop. because when we change str[str.length - 2] it became two true value. we just want one.
+            str.pop()
+            console.log("length", str.length);
+            console.log("str", str);
             stringReduction(str);
             return newStr;
         }
@@ -24,14 +48,18 @@ function stringReduction(str){
 
 }
 
-let str = "aaa"
+let str = ['a', 'b', 'c', 'a', 'a', 'a'];
+let abc = "abc";
 let newStr = [];
 let itemNo = 0;
+ 
+
 //stringReduction(str);
 
 // str = str.slice(0, str.length - 1) // if you want to delete the last item of the string use this.
+//str.pop();
 
 console.log("ezgi", stringReduction(str));
-//console.log(str)
+//console.log(str);
 //console.log(Boolean(str[str.length-1] == str[str.length - 2]));
 
